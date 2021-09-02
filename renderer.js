f8c49bd5377c2d9328dd5eb8bd867b07791842bc
HellSec <57877984+rpie@users.noreply.github.com>
2021-09-01 19:08:29 -0900

    uploaded base files
    THIS IS WIP

---
+++ renderer.js
@@ -0,0 +1,45 @@
+const remote = require('electron').remote;
+
+const win = remote.getCurrentWindow(); 
+
+document.onreadystatechange = (event) => {
+    if (document.readyState == "complete") {
+        handleWindowControls();
+
+        document.getElementById('electron-ver').innerHTML = `${process.versions.electron}`
+    }
+};
+
+window.onbeforeunload = (event) => {
+    win.removeAllListeners();
+}
+
+function handleWindowControls() {
+    document.getElementById('min-button').addEventListener("click", event => {
+        win.minimize();
+    });
+
+    document.getElementById('max-button').addEventListener("click", event => {
+        win.maximize();
+    });
+
+    document.getElementById('restore-button').addEventListener("click", event => {
+        win.unmaximize();
+    });
+
+    document.getElementById('close-button').addEventListener("click", event => {
+        win.close();
+    });
+
+    toggleMaxRestoreButtons();
+    win.on('maximize', toggleMaxRestoreButtons);
+    win.on('unmaximize', toggleMaxRestoreButtons);
+
+    function toggleMaxRestoreButtons() {
+        if (win.isMaximized()) {
+            document.body.classList.add('maximized');
+        } else {
+            document.body.classList.remove('maximized');
+        }
+    }
+}
