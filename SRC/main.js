287cb769c45dff0510d9d4c0b48d0fa17b7f5c00
HellSec <57877984+rpie@users.noreply.github.com>
2021-09-01 19:56:44 -0900

    update
    wooooooo

---
+++ SRC/main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+function createWindow () {
+    mainWindow = new BrowserWindow({
+        width: 900,
+        height: 750,
+        frame: false,
+        transparent: false,
+        icon: 'images/logo.png',
+        backgroundColor: '#1e2025',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
